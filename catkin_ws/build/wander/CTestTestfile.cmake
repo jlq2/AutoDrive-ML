fba61d9d5443177c535194ffeb42eb2ff9562f89
modeloeps <alu@clL029I-11>
2019-02-27 09:52:22 +0100

    gitignore corregido.

--- catkin_ws/build/wander/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alu/Documentos/var/catkin_ws/src/wander
-# Build directory: /home/alu/Documentos/var/catkin_ws/build/wander
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
