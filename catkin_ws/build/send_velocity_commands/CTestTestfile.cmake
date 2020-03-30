8f843455149f4c53a4794240649ba6f56e02db09
Juan Lopez <jlq2.1997@gmail.com>
2020-03-30 17:44:53 +0100

    change repo

--- catkin_ws/build/send_velocity_commands/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/juan/Escritorio/var/catkin_ws/src/send_velocity_commands
-# Build directory: /home/juan/Escritorio/var/catkin_ws/build/send_velocity_commands
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
